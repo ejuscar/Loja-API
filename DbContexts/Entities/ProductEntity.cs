25b811056484d25b2cc35d1f1df500db0ecd1f5c
Eliézer Carmo <eliezerjuscar@gmail.com>
2022-03-16 22:22:15 -0300

    Criação da entidade produto com EF

---
+++ DbContexts/Entities/ProductEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Loja_API.DbContexts.Entities
+{
+    public class ProductEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
---
+++ DbContexts/LojaDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Loja_API.DbContexts.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Loja_API.DbContexts
+{
+    public class LojaDbContext : DbContext
+    {
+        public LojaDbContext()
+        {
+        }
+
+        public LojaDbContext(DbContextOptions<LojaDbContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region Entities
+        public DbSet<ProductEntity> Products { get; set; }
+        #endregion
+    }
+}
